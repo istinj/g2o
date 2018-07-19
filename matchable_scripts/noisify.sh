c687a4eb574b2a11c8208cb7349a769001c7e377
istinj <ialoise@diag.uniroma1.it>
2018-07-19 11:52:25 +0200

    colored scriptoni

--- matchable_scripts/noisify.sh
+++ matchable_scripts/noisify.sh
@@ -5,22 +5,43 @@ if test "$#" -ne 1; then
   exit 1
 fi
 
+# COLORSSSS
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+
+BRED='\033[1;31m'
+BCYAN='\033[1;36m'
+BYELLOW='\033[1;33m'
+BBLUE='\033[1;34m'
+BGREEN='\033[1;32m'
+
+URED='\033[4;31m'
+UCYAN='\033[4;36m'
+UYELLOW='\033[4;33m'
+UBLUE='\033[4;34m'
+UGREEN='\033[4;32m'
+
+NC='\033[0m' # No Color
+
 target_dir=$1
 files=($(ls ${target_dir}))
 
 pwd=`pwd`
 
-echo G2O_ROOT: ${G2O_ROOT}
-echo current directory: $pwd
-echo output directory : ${target_dir}
+echo -e G2O_ROOT: ${UCYAN}${G2O_ROOT}${NC}
+echo -e current directory: ${UCYAN}$pwd${NC}
+echo -e output directory : ${UCYAN}${target_dir}${NC}
 cd ${target_dir}
 echo $'\n'
 
 
 # ia generate low noise graphs
-echo low noise
+echo -e ${BCYAN}low noise${NC}
 if [ -d low_noise ]; then
-  echo cleaning noise directory
+  echo -e ${BRED}cleaning noise directory${NC}
   rm -rf low_noise/
 fi
 mkdir low_noise
@@ -28,7 +49,7 @@ for f in "${files[@]}"; do
   if [ -d ${f} ]; then
       continue
   fi
-  echo processing file ${f}
+  echo -e processing file: ${UCYAN}${f}${NC}
   # get the damn name without extension
   f_base=${f##*/}
   f_prefix=${f_base%.*}
@@ -40,9 +61,9 @@ done
 echo $'\n'
 
 # ia generate mid noise graphs
-echo mid noise
+echo -e ${BCYAN}mid noise${NC}
 if [ -d mid_noise ]; then
-    echo cleaning noise directory
+    echo -e ${BRED}cleaning noise directory${NC}
     rm -rf mid_noise/
 fi
 mkdir mid_noise
@@ -50,7 +71,7 @@ for f in "${files[@]}"; do
   if [ -d ${f} ]; then
       continue
   fi
-  echo processing file ${f}
+  echo -e processing file: ${UCYAN}${f}${NC}
   # get the damn name without extension
   f_base=${f##*/}
   f_prefix=${f_base%.*}
@@ -63,17 +84,17 @@ echo $'\n'
 
 
 # ia generate mid noise graphs
-echo high noise
+echo -e ${BCYAN}high noise${NC}
 if [ -d high_noise ]; then
-  echo cleaning noise directory
-  rm -rf high_noise/
+    echo -e ${BRED}cleaning noise directory${NC}
+    rm -rf high_noise/
 fi
 mkdir high_noise
 for f in "${files[@]}"; do
   if [ -d ${f} ]; then
       continue
   fi
-  echo processing file ${f}
+  echo -e processing file: ${UCYAN}${f}${NC}
   # get the damn name without extension
   f_base=${f##*/}
   f_prefix=${f_base%.*}
---
+++ matchable_scripts/run_noguess.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+
+if test "$#" -ne 1; then
+  echo "Please specify the working directory"
+  exit 1
+fi
+
+# COLORSSSS
+RED='\033[0;31m'
+CYAN='\033[0;36m'
+YELLOW='\033[0;33m'
+BLUE='\033[0;34m'
+GREEN='\033[0;32m'
+
+BRED='\033[1;31m'
+BCYAN='\033[1;36m'
+BYELLOW='\033[1;33m'
+BBLUE='\033[1;34m'
+BGREEN='\033[1;32m'
+
+URED='\033[4;31m'
+UCYAN='\033[4;36m'
+UYELLOW='\033[4;33m'
+UBLUE='\033[4;34m'
+UGREEN='\033[4;32m'
+
+NC='\033[0m' # No Color
+
+
+target_dir=$1
+files=($(ls ${target_dir}))
+
+pwd=`pwd`
+
+echo -e G2O_ROOT: ${UCYAN}${G2O_ROOT}${NC}
+echo -e current directory: ${UCYAN}$pwd${NC}
+echo -e output directory : ${UCYAN}${target_dir}${NC}
+cd ${target_dir}
+echo $'\n'
+
+
+# ia generate stats directory
+if [ -d output ]; then
+  echo -e ${BRED}cleaning ouput directory${NC}
+  rm -rf output/
+fi
+mkdir output
+for f in "${files[@]}"; do
+  #ia skip directories
+  if [ -d ${f} ]; then
+      continue
+  fi
+  
+  echo -e processing file: ${UCYAN}${f}${NC}
+  
+  # get the damn name without extension
+  f_base=${f##*/}
+  f_prefix=${f_base%.*}
+  output_file=output/${f_prefix}_output.g2o
+  stats_file=output/${f_prefix}.stats
+  echo -e output graph: ${UYELLOW}${output_file}${NC}
+  echo -e stats file: ${UYELLOW}${stats_file}${NC}
+
+
+  ${G2O_ROOT}/bin/g2o -v -i 500 -solver lm_var_cholmod -stats ${stats_file} -o ${output_file} ${f}
+  echo $'\n'
+done
+
+echo $'\n'
+echo -e ${BGREEN}done${NC}
+
+cd $pwd
+echo exit
+
+
+
